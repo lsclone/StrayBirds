185926bee7fd553166ea5d62365510da1ef5e783
lsclone <809898816@qq.com>
2015-07-01 13:15:02 +0800

    Update 2015-07-01-intel.cmd

--- _posts/2015-07-01-intel.cmd
+++ _posts/2015-07-01-intel.cmd
@@ -4,5 +4,7 @@ title: 计算机视觉及图形学后续工作
 category: 技术
 ---
 
-1. [opencv wiki 网址](http://wiki.opencv.org.cn/index.php/ "Markdown")
+####后续工作：
+
+1. [opencv wiki网址](http://wiki.opencv.org.cn/index.php/ "Markdown")
 2. [Lock-Free 编程](http://www.tuicool.com/articles/JJRbieR "Markdown")
